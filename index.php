38ee92ed441bf7a4766d361ac486810649ac3812
Marcus Sauceda <marcussauceda@Marcuss-iMac.local>
2022-01-12 19:09:08 -0600

    added conn folder

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db) or ("Couldn't Connect to Database!");
+    require_once("conn/connSocial.php")
 
 
 $firstName = $_GET['firstName'];
@@ -38,7 +28,6 @@ $result = mysqli_query($conn, $query);
         <br/>
         You have successfully reached Index.php in test folder of HTDOCS
     </p>
-        ?>
 
     <div>
         <?php
